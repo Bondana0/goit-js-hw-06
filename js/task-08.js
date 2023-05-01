2b4eebe9b8ddfff44cd141668b48564fcb65181c
Bondana0 <120127799+Bondana0@users.noreply.github.com>
2023-05-01 18:09:52 +0300

    fix

--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,21 @@
-const formEl = document.querySelector(".login-form");
-const loginEl = document.querySelector('[type="email"]');
-const passwordEl = document.querySelector('[type="password"]');
-formEl.addEventListener("submit", handleSubmit);
-function handleSubmit(ev) {
-ev.preventDefault();
-const elements = {
-    password: passwordEl,
-    login: loginEl,
-}
-if(elements.password.value === '' || elements.login.value === ''){
-alert('Введіть login та password :)')
-} else {
-  console.log(`Login: ${elements.login.value }, Password: ${elements.password.value}`);
-  ev.currentTarget.reset();
-}
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return window.alert("Всі поля повинні бути заповнені!");
+  }
+
+  const formData = new FormData(event.currentTarget);
+
+  const result = { email: email.value, password: password.value };
+  console.log(result);
+  event.currentTarget.reset();
 }
--- js/task-10.js
+++ js/task-10.js
@@ -3,31 +3,3 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const divControls = document.querySelector('#controls');
-const divBoxes = document.querySelector('#boxes');
-const btnCreate = document.querySelector('[data-create]');
-const btnDestroy = document.querySelector('[data-destroy]');
-let size = 30;
-const items = []; 
-function createBoxes(amount){
- for(let i = 0; i < amount; i += 1){
- const box = document.createElement("div");
-  box.style.width = `${size}px`;
-  box.style.height = `${size}px`;
-  box.style.background = getRandomHexColor();
-  size += 10;
-  items.push(box); 
-}
-divBoxes.append(...items);
-}
-function destroyBoxes() {
-    divBoxes.innerHTML = '';
-    size = 30;
-  }
-  btnCreate.addEventListener('click', () => {
-         createBoxes(2);
-      }); 
-  btnDestroy.addEventListener('click', () => {
-    destroyBoxes();
-  });
-
